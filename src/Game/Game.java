d9dc9995c694567012e268ed8ce90f7dedde8e05
Jakob-SA <agergaardskovjakob@gmail.com>
2022-10-22 13:14:06 +0200

    Gamestart created
    Took 26 minutes

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,4 +1,13 @@
 package Game;
+import java.util.Scanner;
 
 public class Game {
+    Scanner input = new Scanner(System.in);
+    private void gameStart(String Name1, String Name2) {
+
+
+
+    }
 }
+
+
