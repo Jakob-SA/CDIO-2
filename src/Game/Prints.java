a30ecf6919e406dc0c1fd64d66a916de6013061b
Jakob-SA <agergaardskovjakob@gmail.com>
2022-10-22 13:14:19 +0200

    Prints class created
    Took 16 seconds

---
+++ src/Game/Prints.java
@@ -0,0 +1,11 @@
+package Game;
+
+public class Prints {
+
+    public void player1Name(){
+        System.out.println("Player 1! Write your name");
+    }
+    public void player2Name(){
+        System.out.println("Player 2! Write your name");
+    }
+}
